bf625b978445094d49bcae323bc6d7f2997aa5c6
varaprasad <vara.k.prasadzz@gmail.com>
2018-04-17 12:36:58 +0530

    added property file and modified code

--- src/com/demo/data/One.java
+++ src/com/demo/data/One.java
@@ -1,9 +1,22 @@
 package com.demo.data;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class One {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		FileInputStream fi=new FileInputStream("E://company_workspace//demo//mywebpage.properties");
+		Properties p=new Properties();
+		p.load(fi);
+		WebDriver d=new FirefoxDriver();
+		d.manage().window().maximize();
+		d.get(p.getProperty("url"));
+		d.close();
 
 	}
 
